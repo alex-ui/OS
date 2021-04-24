0ada6eb64d3fc4dce4f7a306424cc1a9a9c98a4b
Peach Corporation <laredoae@gmail.com>
2021-04-24 13:46:33 +0200

    new ver

--- os/compileasm.bat
+++ os/compileasm.bat
@@ -4,7 +4,11 @@ nasm ExtendedProgram.asm -f elf64 -o ExtendedProgram.o
 
 x86_64-w64-mingw32-gcc -ffreestanding -mno-red-zone -m64 -c "Kernel.cpp" -o "Kernel.o"
 
+ld -o Kernel.tmp -Ttext 0x8000 ExtendedProgram.o Kernel.o
+
 ld -T"link.ld"
+
+objcopy -O binary Kernel.tmp Kernel.bin
 
 copy /b bootloader.bin+kernel.bin bootloader.flp
 
